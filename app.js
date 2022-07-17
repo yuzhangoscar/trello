9de7c2f85baf145db36dc3f7140498bf21b4fa07
yuzhangoscar <yuzhang2002@gmail.com>
2022-07-17 18:16:56 +1000

    wip

--- app.js
+++ app.js
@@ -7,5 +7,16 @@ function dragstartHandler(event) {
     event.dataTransfer.setDragImage(img, 10, 10);
 }
 
+function dragoverHandler(event) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+}
+
+function dropHandler(event) {
+    event.preventDefault();
+    const data = event.dataTransfer.getData('text/plain');
+    event.target.appendChild();
+}
+
 draggableOne.addEventListener('dragstart', dragstartHandler);
 draggableOne.addEventListener('dragend', ()=> console.log('dragging ends'));
