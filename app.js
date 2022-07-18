6fbc5bf06df4567145de80432bb8a2d35c0029b7
yuzhang <yuzhang2002@gmail.com>
2022-07-18 19:44:23 +1000

    wwip

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const draggableOne = document.getElementById('draggable-1');
+const draggableTwo = document.getElementById('draggable-2');
+const draggableThree = document.getElementById('draggable-3');
 const dropzoneOne = document.getElementById('dropzone-1');
 
 function dragstartHandler(event) {
-    console.log('start');
     event.dataTransfer.setData('text/plain', event.target.id);
     event.currentTarget.style.backgroundColor = 'yellow';
 }
 
 function dragoverHandler(event) {
-    console.log('dragging over');
     event.preventDefault();
 }
 
 function dropHandler(event) {
     event.preventDefault();
-    console.log('dropped');
     const id = event.dataTransfer.getData('text/plain');
     const draggableElement = document.getElementById(id);
     event.target.appendChild(draggableElement);
@@ -22,5 +21,7 @@ function dropHandler(event) {
 }
 
 draggableOne.addEventListener('dragstart', dragstartHandler);
+draggableTwo.addEventListener('dragstart', dragstartHandler);
+draggableThree.addEventListener('dragstart', dragstartHandler);
 dropzoneOne.addEventListener('dragover', dragoverHandler);
 dropzoneOne.addEventListener('drop', dropHandler);
